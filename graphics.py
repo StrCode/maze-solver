63dc5371809a8743f190642f99853e37c994e81d
vector <samcodebello@gmail.com>
2024-08-07 23:23:23 +0100

    refactored files and added ability to draw lines on the canvas

---
+++ graphics.py
@@ -0,0 +1,50 @@
+from tkinter import BOTH, Tk, Canvas
+
+
+class Window:
+    def __init__(self, width, height) -> None:
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(
+            self.__root, height=height, width=width, background="white"
+        )
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+    def close(self):
+        self.__running = False
+
+
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, point1: Point, point2: Point) -> None:
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas: Canvas, fill_color: str):
+        canvas.create_line(
+            self.point1.x,
+            self.point1.y,
+            self.point2.x,
+            self.point2.y,
+            fill=fill_color,
+            width=2,
+        )
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from window import Window
+from graphics import Window, Line, Point
 
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(30, 30), Point(90, 30))
+    win.draw_line(line, "red")
     win.wait_for_close()
 
 
