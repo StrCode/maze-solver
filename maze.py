a2c5a63ceda286fe5f24a414c6b7f264f830ba2a
vector <samcodebello@gmail.com>
2024-08-17 18:34:46 +0100

    Added the ability to solve the maze

--- maze.py
+++ maze.py
@@ -56,60 +56,125 @@ class Maze:
     def _break_walls_r(self, i: int, j: int):
         self._cells[i][j].visited = True
         while True:
-            next_index_list = []
+            direction_list = []
 
             # determine which cell(s) to visit next
             # left
             if i > 0 and not self._cells[i - 1][j].visited:
-                next_index_list.append((i - 1, j))
+                direction_list.append((i - 1, j))
 
             # right
             if i < self._num_cols - 1 and not self._cells[i + 1][j].visited:
-                next_index_list.append((i + 1, j))
+                direction_list.append((i + 1, j))
 
             # bottom
             if j < self._num_rows - 1 and not self._cells[i][j + 1].visited:
-                next_index_list.append((i, j + 1))
+                direction_list.append((i, j + 1))
 
             # up
             if j > 0 and not self._cells[i][j - 1].visited:
-                next_index_list.append((i, j - 1))
+                direction_list.append((i, j - 1))
 
             # if there is nowhere to go from here
             # just break out
-            if len(next_index_list) == 0:
+            if len(direction_list) == 0:
                 self._draw_cell(i, j)
                 return
 
             # randomly choose the next direction to go
-            direction_index = random.randrange(len(next_index_list))
-            next_index = next_index_list[direction_index]
+            direction_index = random.randrange(len(direction_list))
+            direction = direction_list[direction_index]
 
             # knock out walls between this cell and the next cell(s)
             # right
-            if next_index[0] == i + 1:
+            if direction[0] == i + 1:
                 self._cells[i][j].has_right_wall = False
                 self._cells[i + 1][j].has_left_wall = False
             # left
-            if next_index[0] == i - 1:
+            if direction[0] == i - 1:
                 self._cells[i][j].has_left_wall = False
                 self._cells[i - 1][j].has_right_wall = False
             # down
-            if next_index[1] == j + 1:
+            if direction[1] == j + 1:
                 self._cells[i][j].has_bottom_wall = False
                 self._cells[i][j + 1].has_top_wall = False
             # up
-            if next_index[1] == j - 1:
+            if direction[1] == j - 1:
                 self._cells[i][j].has_top_wall = False
                 self._cells[i][j - 1].has_bottom_wall = False
 
             # recursively visit the next cell
-            self._break_walls_r(next_index[0], next_index[1])
+            self._break_walls_r(direction[0], direction[1])
 
     def _reset_cells_visited(self):
         for col in self._cells:
             for cell in col:
                 cell.visited = False
+
+    def solve(self):
+        return self._solve_r(0, 0)
+
+    def _solve_r(self, i: int, j: int):
+        self._animate()
+
+        self._cells[i][j].visited = True
+
+        lst_col = self._num_cols - 1
+        lst_row = self._num_rows - 1
+        if i == lst_col and j == lst_row:
+            return True
+        direction_list = []
+
+        # determine which cell(s) to visit next
+        # left
+        if i > 0:
+            direction_list.append((i - 1, j))
+
+        # right
+        if i < self._num_cols - 1:
+            direction_list.append((i + 1, j))
+
+        # bottom
+        if j < self._num_rows - 1:
+            direction_list.append((i, j + 1))
+
+        # up
+        if j > 0:
+            direction_list.append((i, j - 1))
+
+        for direction in sorted(direction_list):
+            current_cell: Cell = self._cells[i][j]
+            next_cell: Cell = self._cells[direction[0]][direction[1]]
+            has_wall = True
+
+            # right
+            if direction[0] == i + 1:
+                if not self._cells[i + 1][j].has_left_wall:
+                    has_wall = False
+
+            # left
+            if direction[0] == i - 1:
+                if not self._cells[i - 1][j].has_right_wall:
+                    has_wall = False
+
+            # down
+            if direction[1] == j + 1:
+                if not self._cells[i][j + 1].has_top_wall:
+                    has_wall = False
+
+            # up
+            if direction[1] == j - 1:
+                if not self._cells[i][j - 1].has_bottom_wall:
+                    has_wall = False
+
+            if next_cell and has_wall is False and next_cell.visited is False:
+                current_cell.draw_move(next_cell, False)
+                result = self._solve_r(direction[0], direction[1])
+                if result:
+                    return True
+                current_cell.draw_move(self._cells[direction[0]][direction[1]], True)
+
+        return False
 
     def _animate(self):
         if self._win is None:
